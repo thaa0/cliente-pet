2a68974c1678aed07a7159df079f633c067810fb
Thalita Souza <thalitaoliveira917@gmail.com>
2024-08-01 15:25:15 -0300

    inicia o projeto

---
+++ src/test/java/br/com/pet/cliente_pet/ClientePetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pet.cliente_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
