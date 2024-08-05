1dd150fe153d9804f380b9c734acc038171f472d
Thalita Souza <dev.thalita@gmail.com>
2024-08-05 18:20:07 -0300

    atualizando

--- src/main/java/br/com/pet/clientepet/cliente/domain/Cliente.java
+++ src/main/java/br/com/pet/clientepet/cliente/domain/Cliente.java
@@ -2,9 +2,12 @@ package br.com.pet.clientepet.cliente.domain;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import org.hibernate.validator.constraints.br.CPF;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -14,7 +17,10 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
+@Entity
 public class Cliente {	
+	@Id
+	private UUID idCliente;
 	@NotBlank
 	private String nomeCompleto;	
 	@NotBlank
@@ -38,6 +44,7 @@ public class Cliente {
 			String telefone, Sexo sexo, @NotNull LocalDate dataNascimento, @CPF String cpf,
 			@NotNull Boolean aceitaTermos) {
 		super();
+		this.idCliente = UUID.randomUUID();
 		this.nomeCompleto = nomeCompleto;
 		this.email = email;
 		this.celular = celular;
