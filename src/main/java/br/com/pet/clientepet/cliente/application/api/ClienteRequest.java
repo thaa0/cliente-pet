db3dddd8095312fb50c3a73c33d3d267e4f8989f
Thalita Souza <dev.thalita@gmail.com>
2024-08-08 14:30:55 -0300

    implementa request e response

--- src/main/java/br/com/pet/clientepet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/pet/clientepet/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,30 @@
 package br.com.pet.clientepet.cliente.application.api;
 
-public class ClienteRequest {
+import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.pet.clientepet.cliente.domain.Sexo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class ClienteRequest {
+	@NotBlank
+	private String nomeCompleto;	
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@CPF
+	private String cpf;
+	@NotNull
+	private Boolean aceitaTermos;
 }
