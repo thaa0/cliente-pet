472ab2354909215a4a2d3478af6699b841b2b35a
Thalita Souza <dev.thalita@gmail.com>
2024-08-11 11:40:03 -0300

    implementa fluxo api teste ok

---
+++ src/main/java/br/com/pet/clientepet/cliente/application/api/ClienteListResponse.java
@@ -0,0 +1,12 @@
+package br.com.pet.clientepet.cliente.application.api;
+
+import java.util.UUID;
+
+public class ClienteListResponse {
+	
+	private UUID idCliente;
+	private String nomeCompleto;
+	private String email;
+	private String celular;
+	private String cpf;
+}
