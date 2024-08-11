8b151630c9a0e0258b1be03155eb0155ad5ede64
Thalita Souza <dev.thalita@gmail.com>
2024-08-11 16:40:52 -0300

    implementa fluxo de servico de aplicacao

--- src/main/java/br/com/pet/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/pet/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -2,9 +2,11 @@ package br.com.pet.clientepet.cliente.application.service;
 
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import br.com.pet.clientepet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.pet.clientepet.cliente.application.api.ClienteListResponse;
 import br.com.pet.clientepet.cliente.application.api.ClienteRequest;
 import br.com.pet.clientepet.cliente.application.api.ClienteResponse;
@@ -36,4 +38,12 @@ public class ClienteApplicationService implements ClienteService {
 		log.info("[Finaliza] ClienteApplicationService - buscaTodosCliente");
 		return ClienteListResponse.converte(clientes);
 	}
+
+	@Override
+	public ClienteDetalhadoResponse buscaClienteAtravesId(UUID idCliente) {
+		log.info("[Inicia] ClienteApplicationService - buscaClienteAtravesId");
+		log.info("[Finaliza] ClienteApplicationService - buscaClienteAtravesId");
+		return null;
+	}
+
 }
